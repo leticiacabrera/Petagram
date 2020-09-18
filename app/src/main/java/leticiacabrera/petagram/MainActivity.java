75cdc2cd6909f9b9bfeb9271f1852a30fccecbfe
Leticia Cabrera <leticiac1994@hotmail.com>
2020-09-17 22:15:56 -0300

    Boton con los colores de la app y boton flotante intentando hacerlo

--- app/src/main/java/leticiacabrera/petagram/MainActivity.java
+++ app/src/main/java/leticiacabrera/petagram/MainActivity.java
@@ -3,6 +3,9 @@ package leticiacabrera.petagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.buttonFloating);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
