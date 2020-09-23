4d41c874f670bf016a94af88ab00d9926f02ec7f
Leticia Cabrera <leticiac1994@hotmail.com>
2020-09-23 19:53:41 -0300

    Se agregó una Listview, una Cardview y un .xml llamado dimens

--- app/src/main/java/leticiacabrera/petagram/MainActivity.java
+++ app/src/main/java/leticiacabrera/petagram/MainActivity.java
@@ -1,20 +1,48 @@
 package leticiacabrera.petagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout srlMyIndicatorRefresh;
+    ListView lvMyList;
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        srlMyIndicatorRefresh = (SwipeRefreshLayout) findViewById(R.id.srlMyIndicatorRefresh);
+        lvMyList = (ListView) findViewById(R.id.lvMyList);
+
+
+        String[] planets = getResources().getStringArray(R.array.planets);
+        lvMyList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planets));
+
+        srlMyIndicatorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+            }
+        });
+    }
+
+    public void refreshingContent() {
+        String[] planets = getResources().getStringArray(R.array.planets);
+        lvMyList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planets));
+        srlMyIndicatorRefresh.setRefreshing(false);
     }
 
     public void agregarFAB(){
@@ -22,8 +50,15 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, getResources().getString(R.string.mensaje), Snackbar.LENGTH_SHORT).show();
-                
+                Snackbar.make(view, getResources().getString(R.string.message), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.text_action), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.i("SNACKBAR", "Click in Snackbar");
+                            }
+                        })
+                        .show();
+
             }
         });
     }
